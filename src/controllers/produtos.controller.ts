216e6716dfe4d0c5398919ae418f0b2e9d4ccb22
Thiago <4077420+thiagovf@users.noreply.github.com>
2021-11-08 11:24:58 -0300

    Adicionando models

--- src/controllers/produtos.controller.ts
+++ src/controllers/produtos.controller.ts
@@ -7,33 +7,40 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { Produto } from 'src/model/produto.model';
 
 @Controller('produtos')
 export class ProdutosController {
+  produtos: Produto[] = [
+    new Produto('LIV001', 'Teste Unitários', 29.92),
+    new Produto('LIV002', 'Java para Universitários', 29.93),
+    new Produto('LIV003', 'Rest com NestJS', 29.94),
+  ];
+
   @Get()
-  obterTodos(): string {
-    return 'Lista todos produtos';
+  obterTodos(): Produto[] {
+    return this.produtos;
   }
 
   @Get(':id')
-  obterUmProduto(@Param() params): string {
-    return `Retornar os dados do produto ${params.id}`;
+  obterUmProduto(@Param() params): Produto {
+    return this.produtos[0];
   }
 
   @Post()
-  criarProduto(@Body() produto): string {
+  criarProduto(@Body() produto: Produto) {
     console.log(produto);
-    return 'Produto criado.';
+    this.produtos.push(produto);
   }
 
   @Put()
-  alterarProduto(@Body() produtoAlterado): string {
+  alterarProduto(@Body() produtoAlterado: Produto): Produto {
     console.log(produtoAlterado);
-    return 'Produto alterado.';
+    return produtoAlterado;
   }
 
   @Delete(':id')
   apagarProdut(@Param() params) {
-    return `Apagar produt ${params.id}`;
+    this.produtos.pop();
   }
 }
